1c84e78748d1277df0d665f05f2449dc48cb564d
Willber Nascimento <willberufal@gmail.com>
2021-04-08 20:01:39 -0300

    inicia a criacao do programa em python

---
+++ nvidia-conf/setup-hybrid/hybrid.py
@@ -0,0 +1,113 @@
+import subprocess
+import re
+
+# pacotes necessarios (precisa ver como lidar com linux-lts)
+
+#PACOTES_NECESSARIOS = ['nvidia', 'nvidia-prime', 'nvidia-settings', 'nvidia-utils', 'linux']
+
+PACOTES_NECESSARIOS = ['nvidia', 'linux-lts']
+
+
+# pacotes instalados
+
+PACOTES_INSTALADOS = subprocess.run("pacman -Qn", check=True, capture_output=True, text=True, shell=True)
+PACOTES_INSTALADOS_OUT = PACOTES_INSTALADOS.stdout.split("\n")
+#print(PACOTES_INSTALADOS_OUT)
+
+pacotes_instalados_sv = []
+
+for i in PACOTES_INSTALADOS_OUT:
+    pacotes_instalados_sv.append(re.sub("\s(.*)", "", i))  
+
+
+nao_instalados = []
+for i in PACOTES_NECESSARIOS:
+    if i not in pacotes_instalados_sv:
+        nao_instalados.append(i)
+
+if len(nao_instalados) > 0:
+    print(f'Os pacotes  {nao_instalados} são necessários. Instale-os e tente novamente!')
+
+
+# Configurando o prime
+# PRIME_RENDER_FILE = '10-prime-render-offloader.conf'
+# XORG_CONF_DIR = '/etc/xorg.conf.d/'
+
+PRIME_RENDER_FILE = 'file_teste'
+XORG_CONF_DIR = 'diretorio_teste/'
+
+out_copia_prime_rende_file = subprocess.run(f'cp {PRIME_RENDER_FILE}  {XORG_CONF_DIR}', 
+check=True, 
+capture_output=True, 
+text=True, 
+shell=True)
+
+
+# configurando o gdm (cria mensagem explicando o que acontecendo)
+
+# GDM_CUSTOM_FILE = "custom.conf"
+# GDM_DIR = "/etc/gdm/"
+
+GDM_CUSTOM_FILE = 'file_teste'
+GDM_DIR = 'diretorio_teste/'
+
+out_backup_gdm_custom_file = subprocess.run(f'mv {GDM_DIR}/{GDM_CUSTOM_FILE} {GDM_DIR}/{GDM_CUSTOM_FILE}.prime-bkp', 
+                                            check=True, 
+                                            capture_output=True, 
+                                            text=True, 
+                                            shell=True)
+
+
+out_copia_gdm_custom_file = subprocess.run(f'cp {GDM_CUSTOM_FILE} {GDM_DIR}', 
+                                            check=True, 
+                                            capture_output=True, 
+                                            text=True, 
+                                            shell=True)
+
+
+# xinitrc 
+
+#XINITRC_FILE = "$HOME/.xinitrc"
+XINITRC_FILE = "file_teste"
+status = subprocess.call("test -e '{}'".format(XINITRC_FILE), shell=True)
+
+if status == 0:
+    out_backup_xinitrc_file = subprocess.run(f'mv {XINITRC_FILE} {XINITRC_FILE}.prime-bkp', 
+                                            check=True, 
+                                            capture_output=True, 
+                                            text=True, 
+                                            shell=True)
+                        
+
+
+# cria a imagem do kernel (precisa ver como lidar com linux-lts)
+
+KERNEL_INSTALADO = 'linux' #vai la pra cima essa variavel
+
+out_mkinitcpio = subprocess.run(f'sudo mkinitcpio -p {KERNEL_INSTALADO}', 
+                                            check=True, 
+                                            capture_output=True, 
+                                            text=True, 
+                                            shell=True)
+
+
+
+
+
+# salva arquivo para testar o estado atual da configuracao
+#staus: hybrid-mode
+#       nvidia-only
+#       intel-only
+
+STATUS_FILE = "prime-custom-setup"
+
+subprocess.run(f'echo "hybrid-mode" > $HOME/.{STATUS_FILE}', 
+                                            check=True, 
+                                            capture_output=True, 
+                                            text=True, 
+                                            shell=True) 
+
+# a ideia e testar o status do arquivo. Para idenfificar como proceder
+# na limpeza deles para alterar o estado
+
+# if subprocess.call("test -e '{}'".format(XINITRC_FILE), shell=True)
