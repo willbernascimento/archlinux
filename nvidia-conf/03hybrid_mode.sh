33c59a9bbf1507eb01500eff324c04f5991936d7
Willber Nascimento <willberufal@gmail.com>
2021-04-08 20:01:13 -0300

    simplifica o script

--- nvidia-conf/03hybrid_mode.sh
+++ nvidia-conf/03hybrid_mode.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-# restaura o prime offload
+# configura o prime offload
 
-mv /etc/X11/xorg.conf.d/10-prime-render-offload.conf.bac /etc/X11/xorg.conf.d/10-prime-render-offload.conf
+pacman -S nvidia nvidia-prime nvidia-settings nvidia-utils
 
-mv /etc/modprobe.d/nvidia-drm.conf.bac /etc/modprobe.d/nvidia-drm.conf
+cp 10-prime-render-offload.conf /etc/X11/xorg.conf.d/10-prime-render-offload.conf
+
+
+#mv /etc/X11/xorg.conf.d/10-prime-render-offload.conf.bac /etc/X11/xorg.conf.d/10-prime-#render-offload.conf
+
+#mv /etc/modprobe.d/nvidia-drm.conf.bac /etc/modprobe.d/nvidia-drm.conf
 
 # remove o nvidia-only
 
-mv  /etc/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf /etc/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf.bac
+#mv  /etc/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf /etc/X11/xorg.conf.d/10-nvidia-drm-#outputclass.conf.bac
 
-# Configura o GDM 
+# Configura o GDM: remove o arquivo padrao e desabilita o wayland
 
+cp /etc/gdm/custom.conf /etc/gdm/custom.conf.before.prime
 rm /etc/gdm/custom.conf
-mv /etc/gdm/custom.conf.bac /etc/gdm/custom.conf
+mv custom.conf /etc/gdm/custom.conf
 
-rm /usr/share/gdm/greeter/autostart/optimus.desktop
-rm /etc/xdg/autostart/optimus.desktop
+#rm /usr/share/gdm/greeter/autostart/optimus.desktop
+#rm /etc/xdg/autostart/optimus.desktop
 
 # xinit
 
+cp $HOME/.xinitrc $HOME/.xinitrc.before.prime
 rm $HOME/.xinitrc
 
 mkinitcpio -p linux
+mkinitcpio -p linux-ck
