5fe7647d1871ff8f89d0387a92f6c0f35d7b345e
Willber Nascimento <willberufal@gmail.com>
2021-05-07 00:21:42 -0300

    adiciona pacotes de post-install

--- install-script/arch-post-install-yay.sh
+++ install-script/arch-post-install-yay.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-DESKTOP="gnome-pomodoro-git pop-gtk-theme-git pop-icon-theme-git ttf-roboto-slab firefox-extension-gsconnect gnome-shell-extension-gsconnect"
-INTERNET="skypeforlinux-stable-bin"
-#PROGRAMMING="rstudio-desktop-bin"
+GNOME_EXTENSIONS="gnome-pomodoro-git firefox-extension-gsconnect gnome-shell-extension-gsconnect" 
+THEMES_SHELL="pop-gtk-theme-git pop-icon-theme-git ttf-roboto-slab"
+INTERNET="skypeforlinux-stable-bin zoom phantomjs google-chrome-beta"
+PROGRAMMING="rstudio-desktop-bin visual-studio-code-bin"
+SYSTEM="aic94xx-firmware wd719x-firmware upd72020x-fw udunits touchegg tesseract tesseract-data-por soci hunspell-pt-br"
 
-yay -S --nocleanmenu --nodiffmenu $DESKTOP $INTERNET
+yay -S --nocleanmenu --nodiffmenu $INTERNET $PROGRAMMING
--- install-script/arch-post-install.sh
+++ install-script/arch-post-install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-DESKTOP="gnome gnome-tweaks gnome-software-packagekit-plugin gnome-passwordsafe git neofetch rsync lollypop ttf-fira-sans ttf-fira-mono libreoffice-fresh-pt-br"
+DESKTOP="gnome gnome-tweaks gnome-software-packagekit-plugin gnome-passwordsafe  chrome-gnome-shell geary fragments git neofetch rsync lollypop ttf-fira-sans ttf-fira-mono libreoffice-fresh-pt-br vlc obs-studio"
 
-TERM_CONF="zsh zsh-autosuggestions zsh-completions vim zsh-syntax-highlighting zsh-history-substring-search zsh-theme-powerlevel10k"
+TERM_CONF="zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting zsh-history-substring-search zsh-theme-powerlevel10k"
 
-PROGRAMMING="r gnome-builder gnome-latex texlive-core texlive-latexextra"
+PROGRAMMING="r gnome-builder gnome-latex texlive-core texlive-latexextra texlive-publishers"
 
-INTERNET="firefox firefox-i18n-pt-br telegram-desktop"
+INTERNET="firefox firefox-i18n-pt-br telegram-desktop youtube-dl"
+
+SYSTEM="v4l2loopback-dkms mesa-demos libva-intel-driver libva-utils ffmpegthumbs blueman bluez bluez-utils android-file-transfer android-tools android-udev acpi_call acpi_call-dkms smartmontools gdal"
 
 
 # Install desktop and enable services
 
 pacman -Syu $DESKTOP
 
-SYSTEMD_SERVICES="gdm"
+SYSTEMD_SERVICES="gdm bluetooth smartd"
 
 for i in $SYSTEMD_SERVICES; do
     systemctl enable "${i}"
@@ -21,7 +23,4 @@ done
 
 # Install other pacman packages
 
-pacman -Syu $INTERNET $PROGRAMMING $TERM_CONF
-
-
-
+pacman -Syu $INTERNET $PROGRAMMING $TERM_CONF $SYSTEM
