e7b0ee58ba87f7c5053a48ef5fc411e6ddf61b2a
Willber Nascimento <willberufal@gmail.com>
2021-05-15 11:57:41 -0300

    novos pacotes

--- install-script/arch-post-install-pacman.sh
+++ install-script/arch-post-install-pacman.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DESKTOP="gnome gnome-tweaks gnome-software-packagekit-plugin gnome-passwordsafe  chrome-gnome-shell geary fragments git neofetch rsync lollypop ttf-fira-sans ttf-fira-mono libreoffice-fresh-pt-br vlc obs-studio"
+DESKTOP="gnome gnome-tweaks gnome-software-packagekit-plugin gnome-passwordsafe geary fragments git neofetch rsync lollypop ttf-dejavu  ttf-fira-sans ttf-fira-mono libreoffice-fresh-pt-br vlc obs-studio"
 
-TERM_CONF="zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting zsh-history-substring-search zsh-theme-powerlevel10k"
+TERM_CONF="bash-completion zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting zsh-history-substring-search zsh-theme-powerlevel10k"
 
-PROGRAMMING="r gnome-builder gnome-latex texlive-core texlive-latexextra texlive-publishers"
+PROGRAMMING="r gnome-builder gnome-latex texlive-core texlive-latexextra texlive-publishers texlive-bibtexextra biber texlive-fontsextra"
 
 INTERNET="firefox firefox-i18n-pt-br telegram-desktop youtube-dl"
 
-SYSTEM="v4l2loopback-dkms mesa-demos libva-intel-driver libva-utils ffmpegthumbs blueman bluez bluez-utils android-file-transfer android-tools android-udev acpi_call acpi_call-dkms smartmontools gdal"
+SYSTEM="v4l2loopback-dkms mesa-demos libva-intel-driver libva-utils ffmpegthumbs blueman bluez bluez-utils android-file-transfer android-tools android-udev acpi_call acpi_call-dkms smartmontools gdal gcc-fortran"
 
 
 # Install desktop and enable services
--- install-script/arch-post-install-yay-packages.sh
+++ install-script/arch-post-install-yay-packages.sh
@@ -2,7 +2,7 @@
 
 GNOME_EXTENSIONS="gnome-pomodoro-git firefox-extension-gsconnect gnome-shell-extension-gsconnect" 
 THEMES_SHELL="pop-gtk-theme-git pop-icon-theme-git ttf-roboto-slab"
-INTERNET="skypeforlinux-stable-bin zoom phantomjs google-chrome-beta"
+INTERNET="skypeforlinux-stable-bin zoom phantomjs google-chrome-beta chrome-gnome-shell"
 PROGRAMMING="rstudio-desktop-bin visual-studio-code-bin"
 SYSTEM="aic94xx-firmware wd719x-firmware upd72020x-fw udunits touchegg tesseract tesseract-data-por soci hunspell-pt-br"
 
